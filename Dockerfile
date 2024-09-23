27e69923fae03ad6288d322fd03c4325c5a3d4e7
wangliyang <wangliyang@dobest.com>
2024-09-23 17:49:25 +0800

    build:在Dockerfile中添加条件判断逻辑以设置环境变量添加了一个bash脚本(start.sh)，该脚本根据DRONE_SOURCE_BRANCH的值设置不同的namespace环境变量。

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM harbor.qa.com/common/node:18.16.0-buster-slim
 MAINTAINER wangliyang wangliyang@dobest.com
 WORKDIR /home/yoka
 ADD . .
-RUN npm install --registry=https://registry.npmmirror.com
-
-CMD node server.js
+RUN npm install --registry=https://registry.npmmirror.com \
+     && chmod +x ./start.sh
+ARG DRONE_SOURCE_BRANCH=dev
+ENV DRONE_SOURCE_BRANCH ${DRONE_SOURCE_BRANCH}
+CMD /home/yoka/start.sh && node server.js
---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+branch="$DRONE_SOURCE_BRANCH"
+echo "export group=DEFAULT_GROUP" >> /etc/environment
+echo "-------------------------------"
+echo $DRONE_SOURCE_BRANCH
+echo "-------------------------------"
+#sudo celery multi start -A modules.celery_job.update_data worker
+case $branch in
+"dev")
+  echo "export namespace=1748cc96-8f84-446d-9d75-6506781282d5" >> /etc/environment
+  ;;
+"pre")
+  echo "export namespace=f4387f55-c164-4b0e-9ac0-dd280bb5d93c" >> /etc/environment
+  ;;
+"master")
+  echo "export namespace=9ec3c435-aa26-45d2-b51a-602e094c2b37" >> /etc/environment
+  ;;
+esac
+
